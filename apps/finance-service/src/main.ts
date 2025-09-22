888d26c2d11f5ffcc5eb26a2d2de07268055ded7
LevonGevorgyan <77gevorgyan77@gmail.com>
2025-09-22 14:29:15 +0400

    added finance ms

--- apps/finance-service/src/main.ts
+++ apps/finance-service/src/main.ts
@@ -8,7 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 import * as qs from 'qs';
 import cors from '@fastify/cors';
 
-import { GameModule } from './game.module';
+import { FinanceModule } from './finance.module';
 import { LocalTimeLogger } from 'libs/utils/logger/locale-time-logger';
 import { AppRpcExceptionFilter } from 'libs/utils/interceptors/AppRpcExceptionFilter';
 import { AppExceptionFilter } from 'libs/utils/interceptors/AppExeptionFilter';
@@ -23,7 +23,7 @@ async function bootstrap() {
     await fAdapter.register(cors, { origin: '*' });
 
     const app = await NestFactory.create<NestFastifyApplication>(
-        GameModule,
+        FinanceModule,
         fAdapter,
         { logger: new LocalTimeLogger() },
     );
@@ -37,7 +37,7 @@ async function bootstrap() {
         }),
     );
 
-    app.setGlobalPrefix('games');
+    app.setGlobalPrefix('finance');
     app.useGlobalFilters(new AppRpcExceptionFilter());
 
     // Attach TCP microservice to the SAME app
@@ -45,8 +45,8 @@ async function bootstrap() {
         {
             transport: Transport.TCP,
             options: {
-                host: process.env.GAME_TCP_HOST || '0.0.0.0',
-                port: Number(process.env.GAME_TCP_PORT || 3005),
+                host: process.env.FINANCE_TCP_HOST || '0.0.0.0',
+                port: Number(process.env.FINANCE_TCP_PORT || 3005),
             },
         },
         { inheritAppConfig: true },
@@ -55,7 +55,7 @@ async function bootstrap() {
     app.useGlobalFilters(new AppExceptionFilter());
     // Start both HTTP and TCP
     await app.startAllMicroservices();
-    const httpPort = Number(process.env.GAME_HTTP_PORT || '3010');
+    const httpPort = Number(process.env.FINANCE_HTTP_PORT || '3010');
     await app.listen(httpPort, '0.0.0.0');
 }
 
